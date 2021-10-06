0e975ff47b229b4cc21628b8dd8507a8f0c01d14
Geir Freysson <geir.freysson@gmail.com>
2021-10-06 10:46:12 +0000

    Bump up version to 0.8 for unicom compatability

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
         'numpy',
         'pandas',
     ],
-    version="0.7",
+    version="0.8",
     license='MIT',
     include_package_data=True,
     url="https://github.com/datasmoothie/datasmoothie-tally-client",
