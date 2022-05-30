8c5a08748ec1094e647a59adb061155dd22d8dde
Geir Freysson <geir.freysson@gmail.com>
2022-05-30 14:36:39 +0000

    Version is 0.10

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
         'numpy',
         'pandas',
     ],
-    version="0.9",
+    version="0.10",
     license='MIT',
     include_package_data=True,
     url="https://github.com/datasmoothie/datasmoothie-tally-client",
