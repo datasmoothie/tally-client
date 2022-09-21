c703c526c61d7363fad31659580a942728b13186
Geir Freysson <geir.freysson@gmail.com>
2022-09-21 14:09:14 +0000

    Increase version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
         'numpy',
         'pandas',
     ],
-    version="0.15",
+    version="0.16",
     license='MIT',
     include_package_data=True,
     url="https://github.com/datasmoothie/datasmoothie-tally-client",
