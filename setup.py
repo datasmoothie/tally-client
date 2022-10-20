b599fce7db8e2426cbc45752d3dd402341f81a47
Geir Freysson <geir.freysson@gmail.com>
2022-10-20 13:37:33 +0000

    Release version 0.29

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setuptools.setup(
         'openpyxl',
         'pillow'
     ],
-    version="0.28",
+    version="0.29",
     license='MIT',
     include_package_data=True,
     url="https://github.com/datasmoothie/datasmoothie-tally-client",
