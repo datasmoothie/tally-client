f86edccc1c656a44795cf6db0785c884a0b6b5ff
Geir Freysson <geir.freysson@gmail.com>
2023-01-27 11:07:27 +0000

    Release v 0.39

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
         'pillow',
         'requests'
     ],
-    version="0.38",
+    version="0.39",
     license='MIT',
     include_package_data=True,
     url="https://github.com/datasmoothie/datasmoothie-tally-client",
